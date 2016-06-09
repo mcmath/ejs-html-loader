981fb37cb8f6833f2c6142cb8bd1e50e9ea9ab88
Akim McMath <akim.elijah.mcmath@gmail.com>
2016-06-09 05:54:28 -0700

    Fix: recursive includes
    Fixes #4
    
    Previously, only included statements in the top-level EJS file were
    added as dependencies. This commit adds support for recursive
    dependencies.

--- src/render.js
+++ src/render.js
@@ -1,14 +1,30 @@
+import {readFileSync} from 'fs';
 import {compile} from 'ejs';
 
 export function render(ctx, src, data) {
-  let template = compile(src, {
-    filename: ctx.resourcePath,
-    delimiter: data.delimiter,
-    context: data.context
-  });
+  let filename = ctx.resourcePath;
+  let delimiter = data.delimiter;
+  let context = data.context;
+
+  let tpl = compile(src, {filename, delimiter, context});
 
   return {
-    rendered: template(data),
-    deps: template.dependencies
+    rendered: tpl(data),
+    deps: getDeps(tpl.dependencies, {delimiter, context})
   };
+}
+
+function getDeps(deps, opts, result=[]) {
+  deps.forEach(name => {
+    result.push(name);
+    getDeps(getOwnDeps(name, opts), opts, result);
+  });
+
+  return result;
+}
+
+function getOwnDeps(filename, {delimiter, context}) {
+  let src = readFileSync(filename, 'utf8');
+  let tpl = compile(src, {filename, delimiter, context});
+  return tpl.dependencies;
 }
