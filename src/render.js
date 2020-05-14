83e22206bd16ab80815a995512950b11bd84c20b
Daisuke Inoue <inoue@modelcase.jp>
2020-05-14 14:54:19 +0900

    fix: Cleanup remaining legacy preprocessor include code

--- src/render.js
+++ src/render.js
@@ -9,22 +9,6 @@ export function render(ctx, src, data) {
   let tpl = compile(src, {filename, delimiter, context});
 
   return {
-    rendered: tpl(data),
-    deps: getDeps(tpl.dependencies, {delimiter, context})
+    rendered: tpl(data)
   };
-}
-
-function getDeps(deps, opts, result=[]) {
-  deps.forEach(name => {
-    result.push(name);
-    getDeps(getOwnDeps(name, opts), opts, result);
-  });
-
-  return result;
-}
-
-function getOwnDeps(filename, {delimiter, context}) {
-  let src = readFileSync(filename, 'utf8');
-  let tpl = compile(src, {filename, delimiter, context});
-  return tpl.dependencies;
 }
